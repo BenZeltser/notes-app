fca4af6c0695b1c439d138455b605db12f527e13
Ben Zeltser <zeltserben@Hotmail.com>
2023-11-21 20:07:00 +0200

    Finished Backend programming

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 
+function showNotes() {
+    notesContainer.innerHTML = localStorage.getItem("notes");
+
+}
+
+showNotes();
+
+
+function updateStorage() {
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
+
 createBtn.addEventListener("click", () => {
     let inputBox = document.createElement("p");
     let img = document.createElement("img");
@@ -10,3 +23,20 @@ createBtn.addEventListener("click", () => {
     img.src = "images/delete.png";
     notesContainer.appendChild(inputBox).appendChild(img);
 })
+
+notesContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "IMG") {
+        e.target.parentElement.remove();
+        updateStorage();
+    }
+
+    else if (e.target.tagName === "P"){
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt => {
+            nt.onkeyup = function(){
+                updateStorage();
+            }
+        })
+    }
+})
+
